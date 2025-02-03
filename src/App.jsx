03cff1c5c47c283d0d9e2bdb84f38c76f6e39f5c
adityasemalti <154693138+adityasemalti@users.noreply.github.com>
2025-02-03 21:24:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import Login from './components/Auth/Login'
-import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
-import AdminDashboard from './components/Dashboard/AdminDashboard'
+import EmployeeDashboard from './components/dashboard/EmployeeDashboard'
+import AdminDashboard from './components/dashboard/AdminDashboard'
 import { AuthContext } from './context/AuthProvider'
 
 const App = () => {
